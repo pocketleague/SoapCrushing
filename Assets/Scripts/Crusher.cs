0fa8a1907c39774a4e77c1f03c977f94a032aa12
Siddhesh Dhopatkar <siddho2010@gmail.com>
2020-06-04 23:34:09 +0530

    no message

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Crusher : MonoBehaviour
 {
     public GameObject[] prefab;
+    public Material mat;
+
 
     void Start()
     {
+        prefab[0].GetComponentInChildren<MeshRenderer>().material = mat;
+
         InvokeRepeating("Crush", 1, .01f);
     }
 
@@ -20,9 +24,16 @@ public class Crusher : MonoBehaviour
     {
         if (SingletonClass.instance.IS_CRUSHING)
         {
-            GameObject obj = Instantiate(prefab[Random.Range(0, 6)]);
+//            GameObject obj = Instantiate(prefab[Random.Range(0, 6)]);
+            GameObject obj = Instantiate(prefab[0]);
+
             obj.transform.localPosition = Random.insideUnitSphere * 0.2f + transform.position;
             obj.transform.localScale = Vector3.one * Random.Range(0.7f, 1f);
         }
+    }
+
+    public void ChangeMaterial()
+    {
+
     }
 }
