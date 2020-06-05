5b0b9022eae4a0dfa17315bdd5dc46fa506e2df4
Siddhesh Dhopatkar <siddho2010@gmail.com>
2020-06-05 14:28:17 +0530

    no message

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -21,7 +21,7 @@ public class Crusher : MonoBehaviour
         if (SingletonClass.instance.IS_CRUSHING)
         {
 //            GameObject obj = Instantiate(prefab[Random.Range(0, 6)]);
-            GameObject obj = Instantiate(prefab[1]);
+            GameObject obj = Instantiate(prefab[0]);
 
             obj.transform.localPosition = Random.insideUnitSphere * 0.2f + transform.position;
           //  obj.transform.localScale = Vector3.one * Random.Range(0.7f, 1f);
