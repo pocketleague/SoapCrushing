7a84e003155966f6146c2c47e42362742a289f3e
Siddhesh Dhopatkar <siddho2010@gmail.com>
2020-06-05 12:26:31 +0530

    no message

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -1,23 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Crusher : MonoBehaviour
 {
     public GameObject[] prefab;
-    public Material mat;
+    public Material mat_red, mat_blue;
 
 
     void Start()
     {
-        prefab[0].GetComponentInChildren<MeshRenderer>().material = mat;
 
         InvokeRepeating("Crush", 1, .01f);
-    }
-
-    void Update()
-    {
-
     }
 
     void Crush()
@@ -25,15 +21,20 @@ public class Crusher : MonoBehaviour
         if (SingletonClass.instance.IS_CRUSHING)
         {
 //            GameObject obj = Instantiate(prefab[Random.Range(0, 6)]);
-            GameObject obj = Instantiate(prefab[0]);
+            GameObject obj = Instantiate(prefab[1]);
 
             obj.transform.localPosition = Random.insideUnitSphere * 0.2f + transform.position;
-            obj.transform.localScale = Vector3.one * Random.Range(0.7f, 1f);
+          //  obj.transform.localScale = Vector3.one * Random.Range(0.7f, 1f);
         }
     }
 
-    public void ChangeMaterial()
+    public void ChangeMaterialRed()
     {
+        prefab[1].GetComponentInChildren<MeshRenderer>().material = mat_red;
+    }
 
+    public void ChangeMaterialBlue()
+    {
+        prefab[1].GetComponentInChildren<MeshRenderer>().material = mat_blue;
     }
 }
